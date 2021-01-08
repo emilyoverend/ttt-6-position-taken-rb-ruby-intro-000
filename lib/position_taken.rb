1163161083729f7f74ec4b79919ab497bc83303d
Emily Overend <eo17233@gmail.com>
2021-01-08 03:09:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+board = ["X", "X", "nil", "", "O", "X", "X", " ", "O"]
+
+def position_taken?(board, location)
+  index = board[location.to_i - 1]
+
+  index != " " && index != "" && index != nil
+end
